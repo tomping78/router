e94697fec9693099e6286d6994278e3edbd9458a
geunyoung01~ <screw24@nate.com>
2022-06-15 14:45:29 +0900

    route update

---
+++ src/Pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About(props) {
+    return (
+        <div>
+            <h1>소개</h1>
+            <p>리액트 라우터를 사용해 보는 프로젝트입니다.</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/Pages/Articles.js
@@ -0,0 +1,23 @@
+import { Link, Outlet } from 'react-router-dom';
+
+const Articles = () => {
+  return (
+    <div>
+        
+        <ul>
+        <li>
+            <Link to="/articles/1">게시글 1</Link>
+        </li>
+        <li>
+            <Link to="/articles/2">게시글 2</Link>
+        </li>
+        <li>
+            <Link to="/articles/3">게시글 3</Link>
+        </li>
+        </ul>
+        <Outlet />
+    </div>
+  );
+};
+
+export default Articles;
